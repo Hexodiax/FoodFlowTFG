bacd9eab0f980f513e6e747e6a6b17c58f3ecebc
Hexodiax <104165289+Hexodiax@users.noreply.github.com>
2025-05-08 15:33:13 +0200

    Initial commit

---
+++ app/src/main/java/com/example/foodflowtfg/LoginActivity.java
@@ -0,0 +1,66 @@
+package com.example.foodflowtfg;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.example.foodflowtfg.RegisterActivity;
+import com.google.android.material.button.MaterialButton;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText etEmail, etPassword;
+    private MaterialButton btnLogin;
+    private TextView tvForgotPassword, tvSignUpAction;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Inicializar vistas
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        tvForgotPassword = findViewById(R.id.tvForgotPassword);
+        tvSignUpAction = findViewById(R.id.tvSignUpAction);
+
+        // Evento del botón Login
+        btnLogin.setOnClickListener(v -> {
+            String email = etEmail.getText().toString().trim();
+            String password = etPassword.getText().toString().trim();
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Completa todos los campos", Toast.LENGTH_SHORT).show();
+            } else {
+                //loginUser(email, password);
+            }
+
+        });
+        /**
+         // Evento "Olvidé mi contraseña"
+         tvForgotPassword.setOnClickListener(v -> {
+         startActivity(new Intent(this, ForgotPasswordActivity.class));
+         });
+         **/
+        // Evento "Registrarse"
+        tvSignUpAction.setOnClickListener(v -> {
+            startActivity(new Intent(this, RegisterActivity.class));
+        });
+    }
+
+    private void loginUser(String email, String password) {
+        // Aquí iría tu lógica de autenticación (Firebase, API, etc.)
+        Toast.makeText(this, "Iniciando sesión...", Toast.LENGTH_SHORT).show();
+
+        /**
+         * // Ejemplo de redirección después del login:
+         startActivity(new Intent(this, MainActivity.class));
+         finish();
+         **/
+    }
+}
